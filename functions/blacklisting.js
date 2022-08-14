b64f1292e30c7ac84d78151e1a5d6736f6e83f7f
ruiined <ruiined@gmail.com>
2022-08-14 23:35:31 +0100

    Worked on blacklisting logic while pairing

--- functions/blacklisting.js
+++ functions/blacklisting.js
@@ -3,7 +3,8 @@ import { blacklist } from "../data.js";
 export const userHasBlacklist = (user) =>
   blacklist?.some((obj) => obj.hasOwnProperty(user));
 
-export const findBlacklist = (user) => blacklist.find((obj) => obj[user])[user];
+export const findBlacklist = (user) =>
+  blacklist?.find((obj) => obj[user])[user];
 
 export const updateBlacklist = (user, data) =>
   (blacklist.find((obj) => obj[user])[user] = data);
--- functions/pairGenerator.js
+++ functions/pairGenerator.js
@@ -1,4 +1,12 @@
 import { rejects } from "../data.js";
+import { userHasBlacklist, findBlacklist } from "./blacklisting.js";
+
+const checkBlacklist = (target) =>
+  userHasBlacklist(target) ? findBlacklist(target) : null;
+
+const badMatch = (user, friend) =>
+  !!checkBlacklist(user)?.includes(friend) ||
+  !!checkBlacklist(friend)?.includes(user);
 
 export const pairUp = (people) => {
   const users = [...people].sort(() => Math.random() - 0.5);
@@ -7,14 +15,28 @@ export const pairUp = (people) => {
   const pairs = friends.map((friend) => {
     const index = users[0] !== friend ? 0 : 1;
     const user = users[index] ?? null;
+
+    const findMatch = rejects?.filter((user) => !badMatch(user, friend));
+
     let newPair;
 
-    !user ? rejects?.push(friend) : (newPair = [user, friend]);
+    badMatch(user, friend)
+      ? rejects?.push(user, friend)
+      : !!user
+      ? (newPair = [user, friend])
+      : !rejects?.length && !findMatch.length
+      ? rejects?.push(friend)
+      : (newPair = [findMatch[0], friend]);
+
     friends.splice(friends.indexOf(user), 1);
     users.splice(users.indexOf(friend), 1);
     users.splice(0, 1);
 
+    if (rejects.includes(newPair?.[0]))
+      rejects.splice(rejects.indexOf(newPair[0], 1));
+
     return newPair;
   });
+  // console.log({ pairs, rejects });
   return pairs;
 };
